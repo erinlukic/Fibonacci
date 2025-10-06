16a83f54d878e7d252dc6d5b044a0b2d32a73744
erinlukic <erin.lukic@skole.hr>
2025-10-06 13:45:21 +0200

    Add files via upload

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,18 @@
+﻿// int n = 0;
+int zbroj = 0;
+Console.WriteLine("uneite broj n:");
+n = int.Parse(Console.ReadLine());
+for (int i = 0; i <= n; i++)
+{
+    zbroj = i;
+}
+while (zbroj <= 15)
+{
+    z++;
+    zbroj += z;
+    Console.WriteLine("Broj {0} : , ", zbroj);
+
+}
+           
+                
+Console.WriteLine("Ukupni Fibonnacijev broj je :{0}", zbroj);
